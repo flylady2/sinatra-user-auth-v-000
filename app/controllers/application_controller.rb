4d49483a876d5a9aa2756a3761def101088866ba
Leslie Stevens <flylady@mail.utexas.edu>
2018-10-31 20:34:55 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,8 +16,6 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/registrations' do
-    puts params
-    #binding.pry
     @user = User.new(name: params["name"], email: params["email"], password: params["password"])
     @user.save
     session[:id] = @user.id
@@ -29,8 +27,6 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/sessions' do
-    puts params
-    #binding.pry
     @user = User.find_by(email: params["email"], password: params["password"])
     session[:id] = @user.id
     redirect '/users/home'
